af4487601b63b34d4d8a520f8e7d939842c2ed6f
germancrc <german.cruz36@gmail.com>
2022-09-29 10:51:48 -0400

    fix dash

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 
-function btnRecargar(){
-  document.querySelector("#recarga").style.display = "none";
-}
+// function btnRecargar(){
+//   document.querySelector("#recarga").style.display = "none";
+// }
 
-function clienteHotel() {
-    document.querySelector(
-    "#clienteHotel").style.display = "block";
-    document.querySelector(
-    "#clienteLocal").style.display = "none";
-    document.querySelector(
-    "#cTurista").style.display = "none";
-    document.querySelector(
-      "#cardLocal").style.visibility = "hidden";
-    document.querySelector(
-      "#cardExt").style.visibility = "hidden";
-      document.querySelector("#recarga").style.display = "block";
-}
-function clienteLocal() {
-  document.querySelector(
-    "#clienteHotel").style.display = "none";
-    document.querySelector(
-    "#clienteLocal").style.display = "block";
-    document.querySelector(
-    "#cTurista").style.display = "none";
-    document.querySelector(
-      "#cardHotel").style.visibility = "hidden";
-    document.querySelector(
-      "#cardExt").style.visibility = "hidden";
-      document.querySelector("#recarga").style.display = "block";
-}
-function cTurista() {
-  document.querySelector(
-    "#clienteHotel").style.display = "none";
-    document.querySelector(
-    "#clienteLocal").style.display = "none";
-    document.querySelector(
-    "#cTurista").style.display = "block";
-    document.querySelector(
-      "#cardLocal").style.visibility = "hidden";
-    document.querySelector(
-      "#cardHotel").style.visibility = "hidden";
-      document.querySelector("#recarga").style.display = "block";
-}
+// function clienteHotel() {
+//     document.querySelector(
+//     "#clienteHotel").style.display = "block";
+//     document.querySelector(
+//     "#clienteLocal").style.display = "none";
+//     document.querySelector(
+//     "#cTurista").style.display = "none";
+//     document.querySelector(
+//       "#cardLocal").style.visibility = "hidden";
+//     document.querySelector(
+//       "#cardExt").style.visibility = "hidden";
+//       document.querySelector("#recarga").style.display = "block";
+// }
+// function clienteLocal() {
+//   document.querySelector(
+//     "#clienteHotel").style.display = "none";
+//     document.querySelector(
+//     "#clienteLocal").style.display = "block";
+//     document.querySelector(
+//     "#cTurista").style.display = "none";
+//     document.querySelector(
+//       "#cardHotel").style.visibility = "hidden";
+//     document.querySelector(
+//       "#cardExt").style.visibility = "hidden";
+//       document.querySelector("#recarga").style.display = "block";
+// }
+// function cTurista() {
+//   document.querySelector(
+//     "#clienteHotel").style.display = "none";
+//     document.querySelector(
+//     "#clienteLocal").style.display = "none";
+//     document.querySelector(
+//     "#cTurista").style.display = "block";
+//     document.querySelector(
+//       "#cardLocal").style.visibility = "hidden";
+//     document.querySelector(
+//       "#cardHotel").style.visibility = "hidden";
+//       document.querySelector("#recarga").style.display = "block";
+// }
 

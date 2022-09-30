a6e1f8e1c58313fe2d8617aca3a04a9e4a2ea4c5
germancrc <german.cruz36@gmail.com>
2022-09-30 13:09:06 -0400

    multipleFix

--- js/main.js
+++ js/main.js
@@ -3,19 +3,37 @@
 //   document.querySelector("#recarga").style.display = "none";
 // }
 
-// function clienteHotel() {
-//     document.querySelector(
-//     "#clienteHotel").style.display = "block";
-//     document.querySelector(
-//     "#clienteLocal").style.display = "none";
-//     document.querySelector(
-//     "#cTurista").style.display = "none";
-//     document.querySelector(
-//       "#cardLocal").style.visibility = "hidden";
-//     document.querySelector(
-//       "#cardExt").style.visibility = "hidden";
-//       document.querySelector("#recarga").style.display = "block";
-// }
+function reservaVP() {
+    document.querySelector(
+    "#hardRock").style.display = "none";
+    document.querySelector(
+    "#golfAdmin").style.display = "none";
+    document.querySelector(
+    "#web").style.display = "none";
+    document.querySelector(
+    "#ezlinkWeb").style.display = "none";
+    document.querySelector(
+    "#golfNow").style.display = "none";
+    document.querySelector(
+    "#goGolf").style.display = "none";
+}
+
+function showButtons() {
+    document.querySelector(
+    "#reservaVP").style.display = "block";
+    document.querySelector(
+    "#hardRock").style.display = "block";
+    document.querySelector(
+    "#golfAdmin").style.display = "block";
+    document.querySelector(
+    "#web").style.display = "block";
+    document.querySelector(
+    "#ezlinkWeb").style.display = "block";
+    document.querySelector(
+    "#golfNow").style.display = "block";
+    document.querySelector(
+    "#goGolf").style.display = "block";
+}
 // function clienteLocal() {
 //   document.querySelector(
 //     "#clienteHotel").style.display = "none";

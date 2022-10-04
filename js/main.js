8326d0ec0037e7c5d1ad316bc09849399e057fca
germancrc <german.cruz36@gmail.com>
2022-10-04 18:56:03 -0400

    sidebar mobile

--- js/main.js
+++ js/main.js
@@ -84,5 +84,17 @@ function ezGolf() {
     "#web").style.display = "none";
 }
 
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
   
 

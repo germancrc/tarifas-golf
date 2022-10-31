ece0c423357dfd044f0db8335ce16e373252ddd2
germancrc <german.cruz36@gmail.com>
2022-10-30 21:43:36 -0400

    ignorados

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 
 //servir contenido estatico
 app.use(express.static('public'));

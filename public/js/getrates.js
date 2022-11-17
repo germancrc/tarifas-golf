e383e96878239134ba27085e003109c444dbcfff
germancrc <german.cruz36@gmail.com>
2022-11-17 01:09:39 -0400

    test rate alert

--- public/js/getrates.js
+++ public/js/getrates.js
@@ -1,25 +1,26 @@
-const caddieRate = document.getElementById('caddieCont')
-let ratesHtml = ''
 
-const getCaddie = async () => {
-    await fetch('https://apigolf.cyclic.app/api/tarifas/16')
+//Tarifa Caddie
+function getCaddie() {
+    fetch('https://apigolf.cyclic.app/api/tarifas/16')
       .then((response) => {
         return response.json();
       })
       .then((tarifas) => {
-        console.log(tarifas.data[0].nombre);
-        ratesHtml += 
-        `
-        <p>${tarifas.data[0].nombre}</p>
-        
-        `
+        // console.log(tarifas.data[0].nombre);
+        Swal.fire({
+          title: `Tarifa: ${tarifas.data[0].nombre}`,
+          icon: 'success',
+          html:
+            `<p class="fw-bold text-danger text-center">**Tomar foto al carnet de caddie del cliente**</p>
+             <h4>USD ${tarifas.data[0].precio}.00</h4> `,
+          showCloseButton: true,
+          showCancelButton: false,
+          focusConfirm: true,
+          confirmButtonText:
+          `Entendido`
+        })
+      }
+      )};
+      
 
-  
-
-    }
-    )};
-
-        caddieRate.innerHTML = ratesHtml; //pintar tarifas en HTML
     
-
-getCaddie();

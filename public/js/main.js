05e60247246e04da45cb81bb5b589a2ee606eed8
germancrc <german.cruz36@gmail.com>
2023-01-04 20:53:38 -0400

    button up

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,30 @@ function closeNav() {
 	document.getElementById('mySidenav').style.width = '0'
 }
 
+// ********************************BOTON VOLVER ARRIBA**************************************
+//Get the button
+var mybutton = document.getElementById('btnArriba')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction()
+}
+
+function scrollFunction() {
+	if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+		mybutton.style.display = 'block'
+	} else {
+		mybutton.style.display = 'none'
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0
+	document.documentElement.scrollTop = 0
+}
+// ********************************BOTON VOLVER ARRIBA**************************************
+
 // **********************************SHOW HIDE OPERA CODES WITH CHECBOX***********************
 function checkAllCodes() {
 	let checkbox = document.querySelectorAll('.opera-code-check')

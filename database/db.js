c467ea1f3588cce6a7bfb3f26ab9d28729dea2c0
germancrc <german.cruz36@gmail.com>
2024-04-28 17:25:13 -0400

    Cambio BD provider

--- database/db.js
+++ database/db.js
@@ -1,18 +1,26 @@
-const mysql = require('mysql2')
-const dotenv = require('dotenv')
+const mysql = require("mysql");
 
-dotenv.config()
+const hostname = "382.h.filess.io";
+const database = "golfdb_brokewrote";
+const port = "3307";
+const username = "golfdb_brokewrote";
+const password = "db0ef194e9c44248b38400c9d28c2ea46c0ce2cd";
 
-const connection = mysql.createConnection(process.env.DATABASE_URL, {
-	timezone: 'local',
-})
+const connection = mysql.createConnection({
+  host: hostname,
+  user: username,
+  password: password,
+  database: database,
+  port: port,
+});
 
 connection.connect((error) => {
-	if (error) {
-		console.log('Error de conexion: ' + error)
-		return
-	}
-	console.log('Conectado a DB Planetscale GOLF')
-})
+  if (error) {
+    console.log('Error de conexion: ' + error);
+    return;
+  }
+  console.log('Connected!');
+});
 
-module.exports = connection
+module.exports = connection;
+
